41f2aa3cc82b2b3fba406e2213966ecac8c691ad
albertothinkout <60747961+albertothinkout@users.noreply.github.com>
2020-02-10 15:56:59 +0100

    fix options

--- ios/UdpSockets.m
+++ ios/UdpSockets.m
@@ -53,14 +53,13 @@ RCT_EXPORT_METHOD(createSocket:(nonnull NSNumber*)cId withOptions:(NSDictionary*
 RCT_EXPORT_METHOD(bind:(nonnull NSNumber*)cId
                   port:(int)port
                   address:(NSString *)address
-                  options:(NSDictionary *)options
                   callback:(RCTResponseSenderBlock)callback)
 {
     UdpSocketClient* client = [self findClient:cId callback:callback];
     if (!client) return;
 
     NSError *error = nil;
-    if (![client bind:port address:address options:options error:&error])
+    if (![client bind:port address:address options:nil error:&error])
     {
         NSString *msg = error.localizedFailureReason ?: error.localizedDescription;
         callback(@[msg ?: @"unknown error when binding"]);
